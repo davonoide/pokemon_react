671ce95dd4e0064b61f85c391a02fe4fe7ba6638
davonoide <https://github.com/davonoide/pureba.git>
2024-06-05 19:02:10 -0600

    Grandes cambios

--- src/App.js
+++ src/App.js
@@ -1,26 +1,31 @@
 import React from "react";
+import ReactPlayer from "react-player";
 import Users from './Users/Users';
 import Pokemon from './Pokemons/Pokemons';
-import YouTubeVideo from './VideoYoutube/VideoYoutube';
+import { Grid, Card, CardContent, Typography, CircularProgress, Container, CardActionArea, Button, Box } from "@mui/material";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <Container>
+        
+      <img src="https://media1.tenor.com/m/ZAMoMuQgf9UAAAAC/mapache-pedro.gif" height={60}/>
+      <img src="https://i.imgur.com/8Kx3NEY.jpeg" height={60}/>
       
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      </Container>
+      <Container>
+        <Box display="flex" justifyContent="center" alignItems="center" height="50vh">
+          <ReactPlayer url={"https://www.youtube.com/watch?v=Rsv2VkoIpk0"}/>
+          </Box>
+
+      </Container>
+
+
+      
+      
       </header>
-      <Users />
+      
       <Pokemon />
     </div>
   );
--- src/Pokemons/Pokemons.js
+++ src/Pokemons/Pokemons.js
@@ -1,90 +1,99 @@
-import React, {useState, useEffect} from "react";
-import { Grid, Card, CardContent, Typography, CircularProgress, Container, CardActionArea, Button } from "@mui/material";
+import React, { useState, useEffect } from "react";
+import { Grid, Card, CardContent, Typography, CircularProgress, Container, Button, Box } from "@mui/material";
 
-const Pokemon  = () => {
-
-    const [Pokemons, setPokemons] = useState([]);
-
+const Pokemon = () => {
+    const [pokemons, setPokemons] = useState([]);
     const [loading, setLoading] = useState(true);
-    const[error, setError] = useState(null); //aqui es null porque vamos a manejar el error y el error es un objeto
+    const [error, setError] = useState(null); 
+    const [pokemonSelect, setPokemonSelect] = useState(null); 
 
-    const [PokemonSelect, setPokemonSelect] = useState(null); 
-    useEffect(()=>{
-
+    useEffect(() => {
         const getPokemons = async () => {
-            const response = await fetch('https://pokeapi.co/api/v2/pokemon?limit=20');
-            if(!response.ok){
-                console.log("Error")
-            }else{
+            try {
+                const response = await fetch('https://pokeapi.co/api/v2/pokemon?limit=1025');
+                if (!response.ok) {
+                    throw new Error("Error fetching Pokemons");
+                }
                 const data = await response.json();
                 setPokemons(data.results);
-                console.log(data);
-                //console.log(data.next)
+                setLoading(false);
+            } catch (error) {
+                setError(error);
+                setLoading(false);
             }
-            
-
         };
-
-
         getPokemons();
+    }, []);
 
-    },[]);
-
-    const findPokemonPosition = (name) => {
-        const position = Pokemons.findIndex(pokemon => pokemon.name === name);
-        return position;
-    };
-
-    const fetchDetailPokemon = async(id) =>{
-        //` Backtick
-        const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${id}/`);
-        if(!response.ok){
-            console.log("Error")
-        }else{
+    const fetchDetailPokemon = async (id) => {
+        try {
+            const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${id}/`);
+            if (!response.ok) {
+                throw new Error("Error fetching Pokemon details");
+            }
             const data = await response.json();
             setPokemonSelect(data);
-            console.log(data);
+        } catch (error) {
+            setError(error);
         }
-
     };
+
+    const capitalizeFirstLetter = (string) => {
+        return string.charAt(0).toUpperCase() + string.slice(1);
+    };
+
+    if (loading) {
+        return <CircularProgress />;
+    }
+
+    if (error) {
+        return <Typography variant="h5" color="error">{error.message}</Typography>;
+    }
 
     return (
         <div>
-            <h1>Pokemon list</h1>
-            <ul>
-                {Pokemons.map((Pokemon, index) =>(
-                    <li key = {index}>
-                        <Card variant="outlined">
-                        <CardContent>
-                            <Typography variant="h4" component={"div"}>{Pokemon.name}</Typography> 
-                            <Button color="secondary" variant="contained" onClick={()=>fetchDetailPokemon(index+1)}> Select</Button>
-                            </CardContent>
-                        </Card>
-                    </li>
-                ))}
-            </ul>
-            <hr/>
-            <Typography variant="h2" component={"div"}>Pokemon Detail</Typography>
-            {/* Este elemento aparece solamente cuando PokemonSelect es diferente al NULL*/}
+            <Box sx={{ position: 'sticky', top: 0, zIndex: 1, backgroundColor: 'purple' }}>
+                <Container fixed backgroundColor="grey">
+                    <Typography variant="h2" component="div" color={"white"}>Pokemon Detail</Typography>
+                    {pokemonSelect && (
+                        <div>
+                            <Card variant="outlined">
+                                <CardContent>
+                                    <Typography variant="h5" component="div">Name: {capitalizeFirstLetter(pokemonSelect.name)}.</Typography>
+                                    <img src={pokemonSelect.sprites.front_default} alt={pokemonSelect.name} />
+                                    <Typography variant="h6" component="div">Type: <b>{capitalizeFirstLetter(pokemonSelect.types[0].type.name)}</b></Typography>
+                                    {pokemonSelect.types.length > 1 && (
+                                        <Typography variant="h6" component="div">Type 2: <b>{capitalizeFirstLetter(pokemonSelect.types[1].type.name)}</b></Typography>
+                                    )}
+                                    <Typography variant="h6" component="div">Height: <b>{pokemonSelect.height}</b></Typography>
+                                    <Typography variant="h6" component="div">Weight: <b>{pokemonSelect.weight}</b></Typography>
+                                    <Typography variant="h6" component="div">Pokedex No: <b>{pokemonSelect.id}</b></Typography>
+                                </CardContent>
+                            </Card>
+                        </div>
+                    )}
+                </Container>
+            </Box>
+            <Box backgroundColor={"purple"}>
             <Container>
-            {PokemonSelect && 
-            <div>
-            
-                <Card variant="outlined">
-                    <CardContent>
-                    <Typography variant="h5" component={"div"}>Name:{PokemonSelect.name}</Typography>
-                    <img src= {PokemonSelect.sprites.front_default}/>
-                    <p>Height: {PokemonSelect.height}</p>
-                    <p>Weight: {PokemonSelect.weight}</p>
-                    </CardContent>
-                </Card>
-            </div>} 
+                <Typography variant="h2" component="div" color={"white"}>Pokemon List</Typography>
+                <Grid container spacing={2}>
+                    {pokemons.map((pokemon, index) => (
+                        <Grid item xs={4} key={index}>
+                            <Card variant="outlined">
+                                <CardContent>
+                                    <Typography variant="h4" component="div">{capitalizeFirstLetter(pokemon.name)}</Typography>
+                                    <img src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${index + 1}.png`} alt={pokemon.name} />
+                                    <Button color="secondary" variant="contained" onClick={() => fetchDetailPokemon(index + 1)}>Select</Button>
+                                </CardContent>
+                            </Card>
+                        </Grid>
+                    ))}
+                </Grid>
             </Container>
-            
+            </Box>
         </div>
     );
 };
-
-
 
 export default Pokemon;
